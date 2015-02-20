f178f624f9ad8ac5b9495ee5f56dca4d2f32c8f5
Doupan Guo <doupan@yahoo-inc.com>
2015-02-20 06:49:01 -0800

    initial commit

---
+++ exchange/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  exchange
+//
+//  Created by Doupan Guo on 2/20/15.
+//  Copyright (c) 2015 Doupan Guo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
